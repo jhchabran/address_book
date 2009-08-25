c3358d1dc3f38b5990210795832ee088eca9a7c1
J.H. Chabran <jh@chabran.fr>
2009-08-26 00:07:22 +0200

    added basic spec

--- spec/address_book_spec.rb
+++ spec/address_book_spec.rb
@@ -1,7 +1,15 @@
 require File.expand_path(File.dirname(__FILE__) + '/spec_helper')
 
 describe "AddressBook" do
-  it "fails" do
-    fail "hey buddy, you should probably rename this file and start specing for real"
+  context "implementation" do
+    it "should return an array" do
+      AddressBook.fetch.should be_an(Array)
+    end
+  end
+  
+  context "being used" do 
+    it "should fetch addresses around Paris" do
+      AddressBook.fetch(:city => 'Paris').should have_at_least(1).item
+    end
   end
 end
