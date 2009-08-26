082c4998d405d2bd3b0f08bb801b413a3ceb47b6
J.H. Chabran <jh@chabran.fr>
2009-08-26 12:05:28 +0200

    added basic options

--- lib/address_book.rb
+++ lib/address_book.rb
@@ -3,13 +3,14 @@ require 'rubygems'
 require 'address_book/google'
 
 module AddressBook
-  def self.fetch(opts=default_options)
-    Google.new(opts).fetch
+  def self.fetch(opts={})
+    Google.new(default_options.merge(opts)).fetch
   end
   
   def self.default_options
     {
-      :query => 'Hostel%20Paris',
+      :query => 'Hostel Paris',
+      :min => 3,
       :max => 5,
       :referer => 'localhost:3000'
     }
--- lib/address_book/google.rb
+++ lib/address_book/google.rb
@@ -1,52 +1,72 @@
 require 'rubygems'
 require 'net/http'
+require 'addressable/uri'
 require 'json'
 
 module AddressBook
-  class Google
+  class Google    
     HOST = "ajax.googleapis.com"
     SERVICE_PATH = "/ajax/services/search/local"
-    PARAMETERS = "?v=1.0&q="
+    
+    NotEnoughResultsError = Class.new StandardError
+    InvalidResponseError  = Class.new StandardError
     
     def initialize(opts)
       @opts = opts
+      @url = Addressable::URI.new(:scheme => 'http', :host => HOST, :path => SERVICE_PATH)
     end
     
     def fetch
-      return unless valid_response?
-      data = response['responseData']
-      data['results'].collect do |result|
-        result['addressLines'].to_s
+      @addresses = []
+      
+      query = {:v => '1.0', :start => '0', :rsz => 'large', :q => @opts[:query]}
+      
+      current_path = build_path(@url, query)
+
+      Net::HTTP.start HOST do |http|
+        until(@addresses.length >= @opts[:min]) do 
+          response = http.request_get(current_path, 'Referer' => @opts[:referer])
+          response = parse_json(response)
+          
+          raise NotEnoughResultsError unless enough_results?(response)
+
+          @addresses << collect_addresses(response)
+    
+          current_path = build_path(@url, query.merge(:start => (@addresses.length + 1).to_s))
+        end
+      end
+    
+      @addresses.flatten
+    end
+    
+    def enough_results?(response)
+      response['responseData']['cursor']['estimatedResultCount'].to_i > @opts[:min]
+    end
+    
+    def build_url(url,parameters=nil)
+      url.query_values = parameters if parameters
+      url
+    end
+    
+    def build_path(url, parameters=nil)
+      url = build_url(url, parameters) if parameters
+      url.path + '?' + url.query # there should be a cleaner way to do this
+    end
+    
+    def collect_addresses(response)
+      raise InvalidResponseError unless valid_response? response
+      response['responseData']['results'].collect do |result|
+        result['addressLines'].join(' ')
       end
     end
     
     private
     
-    def valid_response?
+    def valid_response?(response)
       return unless response['responseData']
       return unless response['responseStatus'] == 200
       return unless response['responseData']['results']
       true
-    end
-    
-    def request
-      Net::HTTP.start HOST do |http|
-        response = http.request_get(build_url, 'Referer' => @opts[:referer])
-        # TODO add some http errors handling here
-        parse_json(response)
-      end
-    end
-    
-    def response
-      @response ||= request
-    end
-    
-    def more_result
-      response
-    end
-    
-    def build_url
-      @url ||= SERVICE_PATH + PARAMETERS + @opts[:query]
     end
     
     def parse_json(response)
